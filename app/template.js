5e28ed60d373463e7794a8a739e199ef017e8f79
moh124moh124cry <cherfaouimohamed377@gmail.com>
2026-08-18 13:36:39 +0100

    Update template.js

--- app/template.js
+++ app/template.js
@@ -1,43 +1,9 @@
-import AdminButton from "./adminbutton";
-import AccessBadge from "./accessbadge";
-import AnonymousAuth from "./anonymousauth";
-import WelcomeOverlay from "./welcomeoverlay";
-
 export default function Template({
   children,
 }) {
   return (
     <>
-      <AnonymousAuth />
-      <WelcomeOverlay />
-
       {children}
-
-      <div
-        style={{
-          position: "fixed",
-          top: 82,
-          left: "50%",
-          transform:
-            "translateX(-50%)",
-          zIndex: 9999,
-        }}
-      >
-        <AdminButton />
-      </div>
-
-      <div
-        style={{
-          position: "fixed",
-          bottom: 18,
-          left: "50%",
-          transform:
-            "translateX(-50%)",
-          zIndex: 9998,
-        }}
-      >
-        <AccessBadge />
-      </div>
     </>
   );
 }
